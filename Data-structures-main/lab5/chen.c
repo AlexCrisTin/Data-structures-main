e8480d00fff86ca8e2872e47d6f1bc2a96c66eba
AlexCrisTin <nholaian2222@gmail.com>
2024-07-31 20:49:31 +0700

    zzzz

---
+++ Data-structures-main/lab5/chen.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct node{
+    int data;
+    struct node *next;
+} node;
+
+int main(){
+    node* newnode, *head = NULL, *tail = NULL;
+    int n,k,x, count = 0;
+    scanf("%d %d %d", &n, &k, &x);
+     for(int i = 0 ; i < n + 1 ; i++){
+        newnode = malloc(sizeof(node));
+
+        if(i + 1 == k){
+            newnode->data = x;
+        } else{
+            scanf("%d", &newnode->data);
+        }
+        newnode->next = NULL;
+
+        if(head == NULL){
+            head = newnode;
+            tail = newnode;
+        } else {
+            tail->next = newnode;
+            tail=newnode;
+        }
+     }
+     node *cur = head;
+     while(cur!=NULL){
+        printf("%d ", cur->data);
+        cur = cur->next;
+     }
+}
