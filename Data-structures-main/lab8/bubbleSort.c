cbecd03140529bc620dc8bc0a1d8f3d39f99835b
AlexCrisTin <nholaian2222@gmail.com>
2024-08-05 09:57:21 +0700

    dsfasdf

---
+++ Data-structures-main/lab8/bubbleSort.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+int main(){
+    int n;
+    scanf("%d", &n);
+    int array[n];
+    for(int i = 0 ; i <n;i++){
+        scanf("%d", &array[i]);
+    }
+    int temp;
+    for(int i = 0; i < n - 1;i++){
+        for(int j = 0; j < n - 1-i;j++){
+            if(array[j]>array[j+1]){
+                temp = array[j];
+                array[j]=array[j+1];
+                array[j+1]= temp;
+            }
+        }
+    }
+     for(int i = 0 ; i <n;i++){
+        printf("%d ", array[i]);
+    }
+}
