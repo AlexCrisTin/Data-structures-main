be89acf17205e22c65ef8466544eaa43e1abb8a2
AlexCrisTin <nholaian2222@gmail.com>
2024-08-06 09:18:38 +0700

    asdsdfs

---
+++ Data-structures-main/lab9/train/preoder.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct node{
+    int data;
+    struct node *left,*right;
+}node;
+
+node* newnode(int data){
+    node* temp=(node*)malloc(sizeof(node));
+    temp->data =data;
+    temp->left=temp->right=NULL;
+    return temp;
+}
+node* insert(node* root,int data){
+    if(root == NULL) return newnode(data);
+    if(data<root->data)
+        root->left=insert(root->left, data);
+    else if(data>root->data)
+        root->right=insert(root->right, data);
+    return root;
+}
+
+void preoder(node* root){
+    if(root!=NULL){
+        printf("%d ", root->data);
+        preoder(root->left);
+        preoder(root->right);
+    }
+}
+int main(){
+    int n;
+    scanf("%d", &n);
+    node* root =NULL;
+    for(int i = 0; i < n;i++){
+        int val;
+        scanf("%d", &val);
+        root =insert(root, val);
+    }
+    preoder(root);
+}
