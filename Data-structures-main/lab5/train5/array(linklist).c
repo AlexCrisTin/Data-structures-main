8547d62d06365dd7e3f1901287d40464b818e442
AlexCrisTin <nholaian2222@gmail.com>
2024-08-07 07:15:42 +0700

    asasads

---
+++ Data-structures-main/lab5/train5/array(linklist).c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct node{
+    int data;
+    struct node* next;
+}node;
+
+int main(){
+    node* newnode, *head = NULL, *tail=NULL;
+    int n;
+    scanf("%d", &n);
+    for(int i =0 ;i <n;i++){
+        newnode=malloc(sizeof(node));
+        scanf("%d",&newnode->data);
+        newnode->next=NULL;
+        if(head==NULL){
+            head=newnode;
+            tail=newnode;
+        }else{
+            tail->next=newnode;
+            tail =newnode;
+        }
+    }
+    node* cur = head;
+    while(cur!=NULL){
+        printf("%d ", cur->data);
+        cur = cur->next;
+    }
+}
